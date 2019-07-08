e4f57ffc7b9db11ea3a188be1a407960a48a2af8
Anish Patel <Anish@Anishs-MacBook-Pro.local>
2019-07-08 11:03:03 -0700

    initial setup for webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+};
