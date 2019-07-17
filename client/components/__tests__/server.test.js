66e5ba06fb3a9223781299979b304febe0ac64ff
Anish Patel <Anish@Anishs-MacBook-Pro.local>
2019-07-17 13:55:15 -0700

    added server API testing with supertest

---
+++ client/components/__tests__/server.test.js
@@ -0,0 +1,13 @@
+import '@babel/polyfill';
+
+const express = require('express');
+const request = require('supertest');
+
+const PORT = 2000;
+
+describe('Server API Testing', () => {
+  it('Responds to a GET request', async () => {
+    const res = await request('http://localhost:2000').get('/api/1');
+    expect(res.statusCode).toBe(202);
+  });
+});
