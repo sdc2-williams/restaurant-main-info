ce6dc78bf7679b8dcdab4b570272870920ba37d3
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-24 08:06:16 -0400

    Add Postgres database schema

---
+++ server/db-pg/db.sql
@@ -0,0 +1,14 @@
+drop table if exists restaurants;
+
+create table restaurants (
+  id serial primary key,
+  name varchar(50),
+  description varchar(250),
+  address varchar(250),
+  estDelivery smallint,
+  location numeric(6, 2) [],
+  hours varchar(20) []
+);
+
+-- To load, run:
+-- psql -d [db name] -f db.sql
