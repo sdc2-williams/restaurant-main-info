c51dcf6df391ddecbef80c82c1b938083c30d08f
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-25 17:03:20 -0400

    Add test table to schema

--- server/db-pg/schema.sql
+++ server/db-pg/schema.sql
@@ -10,5 +10,17 @@ create table restaurants (
   hours varchar(20) []
 );
 
+drop table if exists rest_test;
+
+create table rest_test (
+  id serial primary key,
+  name varchar(50),
+  description varchar(300),
+  address varChar(250),
+  estDelivery smallint
+  -- location text,
+  -- hours text
+);
+
 -- To load, run:
 -- psql -d [db name] -f schema.sql
