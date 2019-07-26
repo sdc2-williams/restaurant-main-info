309b1af28cb9f0469af2ea2e3dc58be95e79d890
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-26 14:12:45 -0400

    Implement toy Cassandra query

---
+++ server/db-cassandra/index.js
@@ -0,0 +1,8 @@
+const cd = require('cassandra-driver');
+
+const client = new cd.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'test' });
+
+const queryString = 'select * from restaurants';
+client.execute(queryString)
+  .then(res => console.log(res.rows))
+  .catch(console.error);
