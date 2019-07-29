bf843e83bf0f7de8fdc922eaf06d3aa45ff69ffc
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2019-07-29 15:13:17 -0400

    Fix old, incorrect test

--- client/components/__tests__/server.test.js
+++ client/components/__tests__/server.test.js
@@ -8,6 +8,6 @@ const PORT = 2000;
 describe('Server API Testing', () => {
   it('Responds to a GET request', async () => {
     const res = await request('http://localhost:2000').get('/api/restaurant/1');
-    expect(res.statusCode).toBe(202);
+    expect(res.statusCode).toBe(200);
   });
 });
